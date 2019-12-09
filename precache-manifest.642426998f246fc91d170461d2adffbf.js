5d1b91a290a4f28b31c07d0809f33814200d5b2a
better36524 <better36524@gmail.com>
2019-12-09 22:24:52 +0900

    Updates

---
+++ precache-manifest.642426998f246fc91d170461d2adffbf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0228a986e09659075f20593095456cc",
+    "url": "/practice_react_sample_todo_basic/index.html"
+  },
+  {
+    "revision": "3ec29d99dc37d7311b4e",
+    "url": "/practice_react_sample_todo_basic/static/css/main.fd0e6774.chunk.css"
+  },
+  {
+    "revision": "5fa27449637a0c6bb57c",
+    "url": "/practice_react_sample_todo_basic/static/js/2.4a240699.chunk.js"
+  },
+  {
+    "revision": "8a14dcea7ad8b593c1e83c11f7ba1a89",
+    "url": "/practice_react_sample_todo_basic/static/js/2.4a240699.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3ec29d99dc37d7311b4e",
+    "url": "/practice_react_sample_todo_basic/static/js/main.b19a72e2.chunk.js"
+  },
+  {
+    "revision": "e504c04a8ab92ad1ec51",
+    "url": "/practice_react_sample_todo_basic/static/js/runtime-main.d6781d3b.js"
+  }
+]);
